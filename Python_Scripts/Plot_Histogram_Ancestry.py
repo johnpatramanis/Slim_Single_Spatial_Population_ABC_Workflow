d10eb35b8f0751a33b292e77129e726ae06eaee5
Ioannis Patramanis <32954442+johnpatramanis@users.noreply.github.com>
2026-02-17 15:12:42 +0100

    Add files via upload

--- Python_Scripts/Plot_Histogram_Ancestry.py
+++ Python_Scripts/Plot_Histogram_Ancestry.py
@@ -1,9 +1,10 @@
 ##### Takes input of "Find_Admixture.py"
-##### Run like this """ python Plot_Histogram_Ancestry.py ./Simulation_Runs/Simulation_0/Tracks ./Simulation_Runs/Simulation_0/Ancestry_Plots """
+##### Run like this """ python Python_Scripts/Plot_Histogram_Ancestry.py ./Simulation_Runs/Simulation_0/Tracks ./Simulation_Runs/Simulation_0/Ancestry_Plots """
 
 ### Import Packages
 import sys
 import os
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -22,11 +23,12 @@ Genomewide_Ancestries = File.readline().strip().split(':')[1]
 Genomewide_Ancestries = Genomewide_Ancestries.split(',')
 Genomewide_Ancestries = [x for x in Genomewide_Ancestries if x!=''] ### Cleanup empty ancestries
 
+
 #### Create Colour Palette
 Colours_to_ancestries = {}
 if len(Genomewide_Ancestries) <= 4:
     Colours_to_ancestries = {'1':'xkcd:deep blue','2':'xkcd:pinkish','3':'xkcd:soft green','4':'xkcd:lemon yellow'} ###### https://xkcd.com/color/rgb/
- 
+    Colour_Maps_to_Ancestries = {'1':'Blues','2':'Reds','3':'Greens','4':'Wistia'}
 
  
 if len(Genomewide_Ancestries) > 4:
@@ -34,7 +36,8 @@ if len(Genomewide_Ancestries) > 4:
     for x in range(0,len(Genomewide_Ancestries)):
         colorz = tuple(np.random.random(size=3))
         Colours_to_ancestries[ Genomewide_Ancestries[x] ] = colorz
-        
+        Colour_Maps_to_Ancestries[ Genomewide_Ancestries[x] ] = 'viridis'
+
 
 #### Read info on sampled individuals
 Individual_Info = []
@@ -46,7 +49,7 @@ for LINE in Individuals_File:
     LINE = LINE.strip().split()
     Individual_Info.append([ LINE[0], LINE[1], LINE[2] , LINE[3], LINE[4], LINE[5], LINE[6] , LINE[7] ])
     Location = [float(x) for x in LINE[1].split('--')]
-    Individual_Location.append( [LINE[0]] + Location)
+    Individual_Location.append( [LINE[0]] + Location )
 
 Individual_Location = sorted(Individual_Location, key = lambda x: x[1])
 
@@ -67,6 +70,7 @@ for line in File:
     Ancestries = [ float(x) for x in Ancestries if x!='']
     Ancestries = [ (x/sum(Ancestries)) for x in Ancestries]
     Individuals_to_Ancestries[Ind_ID] = Ancestries
+    
 
 
 
@@ -77,6 +81,16 @@ for x in Individual_Location:
     for y in Individuals_to_Ancestries.keys():
         if x[0] in y:
             Sorted_IDs.append(y)
+
+
+
+
+
+
+
+
+
+
 
 
 ################################### Plotting
@@ -101,5 +115,108 @@ plt.savefig(F"{Output_Folder}/Ancestry_Barplot.pdf")
 
 
 
-#### Histogram
 
+
+
+
+
+
+
+
+#### Painted Map
+
+
+##### Figure out best width and height for this group of individuals
+max_width = round(max(Individual_Location, key = lambda x: x[1])[1])### maximum X axis position, rounded
+max_height = round(max(Individual_Location, key = lambda x: x[2])[2]) ### maximum Y axis position, rounded
+
+## add a bit until its divisable by 5 (arbitary, but makes plots nicer)
+while max_height%5 !=0:
+    max_height+=1
+
+while max_width%5 !=0:
+    max_width+=1
+
+print(F"Maximum width for this space is: {max_width} and maximum height is: {max_height}\n")
+
+
+
+##### Create spatial boxes of 5x5
+
+N_X_Boxes = int(max_width/5) ## Number of bins/boxes in X axis
+N_Y_Boxes = int(max_height/5) ## Number of bins/boxes in Y axis
+N_Boxes = N_X_Boxes * N_Y_Boxes ## Number of total bins/boxes
+
+##### Seperate all individuals into spatial boxes of 5x5
+
+BOXES = []
+
+    
+for X_DIM in range(5,max_width+5,5):
+    for Y_DIM in range(5,max_height+5,5):
+        
+        BOX_HERE = [ ]
+        
+        for IND in Individual_Location:
+            
+            IND_X = IND[1]
+            IND_Y = IND[2]
+            
+            HAPS = [x for x in Individuals_to_Ancestries.keys() if (IND[0] == x.split('_')[1] ) ] ## haplotypes of individual
+            
+            if (IND_X >= X_DIM-5) and (IND_Y >= Y_DIM-5) and (IND_X < X_DIM) and (IND_Y < Y_DIM):
+                
+                for HAP in HAPS:
+                    BOX_HERE.append(Individuals_to_Ancestries[HAP]) ### ancestry proportions of each haplotype
+
+
+        ### if no individual sampled in box, add an individual with 0 ancestry for every ancestry
+        if BOX_HERE == []:
+            BOX_HERE.append( [0 for x in Ancestries] )
+            
+        ### add this 5x5 box to the totality of boxes    
+        BOXES.append(BOX_HERE)
+        
+
+ 
+for ANCESTRY_IN_QUESTION in range(0,len(Ancestries)):
+    
+    ANCESTRY_IN_BOXES = [ float(np.mean([y[ANCESTRY_IN_QUESTION] for y in x ])) for x in BOXES ] ### get average ancestry in question for each box
+    ### ^^Should be a list of floats, length == N_Boxes
+    ### print(ANCESTRY_IN_BOX,len(ANCESTRY_IN_BOX))
+    Colour_Map_Name = Colour_Maps_to_Ancestries[Genomewide_Ancestries[ANCESTRY_IN_QUESTION]]
+    
+
+    Z = []
+    counter = 0
+    
+    
+    for X_DIM in range(5,max_width+5,5):
+        X_ROW = []
+        
+        for Y_DIM in range(5,max_height+5,5):
+            
+            X_ROW.append(ANCESTRY_IN_BOXES[counter])
+            counter += 1
+            
+        Z.append(X_ROW)    
+        
+    FOR_PLOTTING_X = [ X_DIM - 2.5 for X_DIM in range(0,max_width+5,5)  ]
+    FOR_PLOTTING_Y = [ Y_DIM - 2.5 for Y_DIM in range(0,max_height+5,5) ]
+
+    
+    
+    X,Y = np.meshgrid(np.array(FOR_PLOTTING_X), np.array(FOR_PLOTTING_Y))
+    Z = np.array(Z)
+    
+    
+    fig, ax = plt.subplots()
+    C = ax.pcolormesh(X, Y , Z.T, shading = 'auto', rasterized = True, cmap = Colour_Map_Name)
+    
+    plt.title(F"Spatial Distribution of Ancestry {Genomewide_Ancestries[ANCESTRY_IN_QUESTION]}")
+    plt.xlabel('Position on X axis', fontweight ='bold', fontsize = 13)
+    plt.ylabel('Position on Y axis', fontweight ='bold', fontsize = 13)
+    fig.colorbar(C)
+
+    #### plt.legend()
+    plt.savefig(F"{Output_Folder}/Ancestry_{Genomewide_Ancestries[ANCESTRY_IN_QUESTION]}_Spatial_Distribution.pdf")
---
+++ Python_Scripts/Plot_Histogram_Matching_Haplotypes.py
@@ -0,0 +1,327 @@
+##### Takes input of "Find_Admixture.py"
+##### Run like this """ python Plot_Histogram_Matching_Haplotypes.py ./Simulation_Runs/Simulation_0/Diversity_Metrics ./Simulation_Runs/Simulation_0/Ancestry_Plots """
+
+### Import Packages
+import sys
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+
+########## Load and organise data
+### Folders
+Folder = sys.argv[1]
+Output_Folder = sys.argv[2]
+File = 'Ancestry_Sharing.txt'
+File = open(F"{Folder}/{File}",'r')
+
+#### Read first line, get info
+Labels = File.readline().strip().split()
+
+
+
+###################### Load data, organise it to pairs of individuals
+
+ANCESTRIES = {}
+
+for Line in File:
+    
+    Line = Line.strip().split()
+    
+    ## Load columns
+    Chromosome = Line[0]
+    Ancestry = Line[1]
+    ID1 = Line[2]
+    ID2 = Line[3]
+    ID2 = Line[3]
+    Similarity = float(Line[8])
+    
+    
+    ## Organise
+    
+    IDS = '-'.join(sorted([ID1,ID2]))
+    
+    ### Filter chromosomes
+    if Chromosome in ['chromosome_Y','chromosome_MT']:
+        continue
+    ### not filtered are added into dictionary of ancestries and pairs
+    if Chromosome not in ['chromosome_Y','chromosome_MT']:
+       
+        if Ancestry not in ANCESTRIES.keys():
+            
+            ANCESTRIES[Ancestry] = {IDS:[Similarity]}
+            
+            
+        if Ancestry in ANCESTRIES.keys(): 
+            
+            if IDS not in ANCESTRIES[Ancestry].keys():
+                
+                ANCESTRIES[Ancestry][IDS] = [Similarity]
+            
+            if IDS in ANCESTRIES[Ancestry].keys():
+                
+                ANCESTRIES[Ancestry][IDS].append(Similarity)
+
+
+
+
+
+
+
+
+######################
+## Cleanup, have 1 metric for each pair of individuals
+
+DATA = []
+SAMPLES = []
+for ANC in sorted(ANCESTRIES.keys()):
+
+    for IDS in sorted(ANCESTRIES[ANC].keys()):
+        
+        ANCESTRIES[ANC][IDS] = np.mean(ANCESTRIES[ANC][IDS])
+        DATA.append([ANC,IDS,ANCESTRIES[ANC][IDS]])
+        
+        IDS = IDS.split('-')
+        for ID in IDS:
+            if ID not in SAMPLES:
+                SAMPLES.append(ID)
+
+
+
+######################
+### Load Location of individuals (their haplosomes)
+#### Read info on sampled individuals
+
+Individual_Info = []
+Individual_Location = []
+Individuals_File =   open(F"{Folder.replace("Diversity_Metrics","Sampled_Individuals.txt")}",'r')      
+Ind_Headers = Individuals_File.readline().strip().split()
+
+for LINE in Individuals_File:
+    LINE = LINE.strip().split()
+    Individual_Info.append([ LINE[0], LINE[1], LINE[2] , LINE[3], LINE[4], LINE[5], LINE[6] , LINE[7] ])
+    Location = [float(x) for x in LINE[1].split('--')]
+    Individual_Location.append( [LINE[0]] + Location )
+
+Individual_Location = sorted(Individual_Location, key = lambda x: x[1])
+
+
+print(Individual_Location)
+
+
+
+
+
+
+
+
+
+
+
+
+###################### Plot Distance matrix style
+
+#### Sorted based on position on X axis
+
+
+
+for ANC in sorted(ANCESTRIES.keys()):
+    
+    SORTED_BY_LOCATION_DATA = []
+    RE_SORTED_BY_LOCATION_DATA = []
+    
+    DATA_THIS_ANCESTRY = [ x for x in DATA if x[0] == ANC ]
+    
+    
+    #### loop through IDs, sorted by location on the X axis
+    for LOC_INFO in Individual_Location:
+    
+        ID_NAME = LOC_INFO[0]
+        LOCATION = LOC_INFO[1]
+        ID_NAME_LIST = []
+        
+        for DATA_HERE in DATA_THIS_ANCESTRY:
+            
+            PAIR = DATA_HERE[1]
+            PAIR = sorted(PAIR.split('-'))
+            ID1 = PAIR[0].split('_')[1]
+            ID2 = PAIR[1].split('_')[1] 
+            
+            if ( ( ID1 == ID_NAME ) or ( ID2 == ID_NAME ) ) and (DATA_HERE not in SORTED_BY_LOCATION_DATA):
+                
+                SORTED_BY_LOCATION_DATA.append(DATA_HERE)
+                
+                
+                
+                
+                
+                
+                ### For second sorting
+                ID_NAME_LIST.append([DATA_HERE,LOCATION])
+                
+                
+                
+                
+                
+        #### Second sorting needed for Y axis
+
+        ID_NAME_LIST = sorted(ID_NAME_LIST, key=lambda x: x[1])
+
+        for TEMP in ID_NAME_LIST:
+            RE_SORTED_BY_LOCATION_DATA.append(TEMP[0])
+        
+        
+        
+    
+    Max_Dim = len(SAMPLES)
+    Sim_matrix = np.zeros((Max_Dim,Max_Dim))
+
+
+
+    FINAL_MATRIX = [x[2] for x in RE_SORTED_BY_LOCATION_DATA]
+    
+    counter=0
+    for i in range(0,Max_Dim):
+        for j in range(i+1, Max_Dim):
+
+            Sim_matrix[i,j] = FINAL_MATRIX[counter]
+            Sim_matrix[j,i] = Sim_matrix[i,j]
+            counter+=1
+
+    
+
+    
+    plt.imshow(Sim_matrix, cmap='hot', interpolation='nearest')
+    plt.show()    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+##################### Plotting in a 2D space
+
+
+
+#### Create Colour Palette
+# Colours_to_ancestries = {}
+# if len(Genomewide_Ancestries) <= 4:
+    # Colours_to_ancestries = {'1':'xkcd:deep blue','2':'xkcd:pinkish','3':'xkcd:soft green','4':'xkcd:lemon yellow'} ###### https://xkcd.com/color/rgb/
+ 
+
+ 
+# if len(Genomewide_Ancestries) > 4:
+
+    # for x in range(0,len(Genomewide_Ancestries)):
+        # colorz = tuple(np.random.random(size=3))
+        # Colours_to_ancestries[ Genomewide_Ancestries[x] ] = colorz
+        
+
+
+###################################################################################
+### Organise Individuals in space
+
+
+#### Read info on sampled individuals
+
+# Individual_Info = []
+# Individual_Location = []
+# Individuals_File_Name = Folder.replace("Diversity_Metrics","Sampled_Individuals.txt")
+# Individuals_File = open(Individuals_File_Name,'r')      
+# Ind_Headers = Individuals_File.readline().strip().split()
+
+# for LINE in Individuals_File:
+    # LINE = LINE.strip().split()
+    # Individual_Info.append([ LINE[0], LINE[1], LINE[2] , LINE[3], LINE[4], LINE[5], LINE[6] , LINE[7] ]) #### 
+    # Location = [float(x) for x in LINE[1].split('--')]
+    # Individual_Location.append( [LINE[0]] + Location)
+
+# Individual_Location = sorted(Individual_Location, key = lambda x: x[1]) ### Sorted by position on X axis
+
+
+
+
+
+##### Figure out best width and height for this group of individuals
+# max_width = round(max(Individual_Location, key = lambda x: x[1])[1])### maximum X axis position, rounded
+# max_height = round(max(Individual_Location, key = lambda x: x[2])[2]) ### maximum Y axis position, rounded
+
+## add a bit until its divisable by 5 (arbitary, but makes plots nicer)
+# while max_height%5 !=0:
+    # max_height +=1
+    
+# while max_width%5 !=0:
+    # max_width +=1
+    
+# print(F"Maximum width for this space is: {max_width} and maximum height is: {max_height}\n")
+
+
+
+##### Create spatial boxes of 5x5
+
+# N_X_Boxes = int(max_width/5) ## Number of bins/boxes in X axis
+# N_Y_Boxes = int(max_height/5) ## Number of bins/boxes in Y axis
+# N_Boxes = N_X_Boxes * N_Y_Boxes ## Number of total bins/boxes
+
+##### Seperate all individuals into spatial boxes of 5x5
+
+# BOXES = []
+
+    
+# for X_DIM in range(5,max_width+5,5):
+    # for Y_DIM in range(5,max_height+5,5):
+        
+        # BOX_HERE = []
+        
+        # for IND in Individual_Location:
+            # IND_X = IND[1]
+            # IND_Y = IND[2]
+        
+            # if (IND_X >= X_DIM-5) and (IND_Y >= Y_DIM-5) and (IND_X < X_DIM) and (IND_Y < Y_DIM):
+                
+                # BOX_HERE.append(IND)
+
+        # BOXES.append(BOX_HERE)
+        
+        
+# print(BOXES,len(BOXES))
