82cd0ae04afb460a51b08644253f041d9f5eb186
Ioannis Patramanis <32954442+johnpatramanis@users.noreply.github.com>
2026-08-31 11:55:39 +0200

    Add files via upload

--- Python_Scripts/Plot_Histogram_Matching_Haplotypes_Composite_Individuals.py
+++ Python_Scripts/Plot_Histogram_Matching_Haplotypes_Composite_Individuals.py
@@ -8,24 +8,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-
-
 ########## Load and organise data
 ### Folders
 Folder = sys.argv[1]
 Output_Folder = sys.argv[2]
 
-for Box_Size_File in os.listdir(F"{Folder}"):
+for Box_Size_File in os.listdir(f"{Folder}"):
 
-    File = open(F"{Folder}/{Box_Size_File}",'r')
+    File = open(f"{Folder}/{Box_Size_File}", 'r')
 
     #### Read first line, get info
     Labels = File.readline().strip().split()
 
-
-
     ###################### Load data, organise it to pairs of individuals
-
     ANCESTRIES = {}
 
     for Line in File:
@@ -39,42 +34,27 @@ for Box_Size_File in os.listdir(F"{Folder}"):
         ID2 = Line[3]
         Similarity = float(Line[8])
         
-        
         ## Organise
-        
-        IDS = '-'.join(sorted([ID1,ID2]))
+        IDS = '-'.join(sorted([ID1, ID2]))
         
         ### Filter chromosomes
-        if Chromosome in ['chromosome_Y','chromosome_MT']:
+        if Chromosome in ['chromosome_Y', 'chromosome_MT']:
             continue
         ### not filtered are added into dictionary of ancestries and pairs
-        if Chromosome not in ['chromosome_Y','chromosome_MT']:
-           
+        if Chromosome not in ['chromosome_Y', 'chromosome_MT']:
+            
             if Ancestry not in ANCESTRIES.keys():
-                
-                ANCESTRIES[Ancestry] = {IDS:[Similarity]}
-                
+                ANCESTRIES[Ancestry] = {IDS: [Similarity]}
                 
             if Ancestry in ANCESTRIES.keys(): 
-                
                 if IDS not in ANCESTRIES[Ancestry].keys():
-                    
                     ANCESTRIES[Ancestry][IDS] = [Similarity]
                 
                 if IDS in ANCESTRIES[Ancestry].keys():
-                    
                     ANCESTRIES[Ancestry][IDS].append(Similarity)
-
-
-
-
-
-
-
 
     ######################
     ## Cleanup, have 1 metric for each pair of individuals
-
     DATA = []
     SAMPLES = []
     for ANC in sorted(ANCESTRIES.keys()):
@@ -82,7 +62,7 @@ for Box_Size_File in os.listdir(F"{Folder}"):
         for IDS in sorted(ANCESTRIES[ANC].keys()):
             
             ANCESTRIES[ANC][IDS] = np.mean(ANCESTRIES[ANC][IDS])
-            DATA.append([ANC,IDS,ANCESTRIES[ANC][IDS]])
+            DATA.append([ANC, IDS, ANCESTRIES[ANC][IDS]])
             
             IDS = IDS.split('-')
             for ID in IDS:
@@ -91,30 +71,26 @@ for Box_Size_File in os.listdir(F"{Folder}"):
 
     #####################
     ## Assign Location to each Composite individual
-
     Individual_Location = []
 
     for SAMPLE in SAMPLES:
         Location = SAMPLE.split("_")
         Location = Location[len(Location)-2:len(Location)]
         Location = [float(x) for x in Location]
-        Individual_Location.append( [ SAMPLE ] + Location )
+        Individual_Location.append([SAMPLE] + Location)
 
-    Individual_Location = sorted(Individual_Location, key = lambda x: x[1])
-
-
+    Individual_Location = sorted(Individual_Location, key=lambda x: x[1])
 
 
-###################### Plot Distance matrix style
-#### Sorted based on position on X axis
+    ###################### Plot Distance matrix style
+    #### Sorted based on position on X axis
 
     for ANC in sorted(ANCESTRIES.keys()):
         
         SORTED_BY_LOCATION_DATA = []
         RE_SORTED_BY_LOCATION_DATA = []
         
-        DATA_THIS_ANCESTRY = [ x for x in DATA if x[0] == ANC ]
-        
+        DATA_THIS_ANCESTRY = [x for x in DATA if x[0] == ANC]
         
         ## loop through IDs, sorted by location on the X axis
         for LOC_INFO in Individual_Location:
@@ -122,7 +98,6 @@ for Box_Size_File in os.listdir(F"{Folder}"):
             ID_NAME = LOC_INFO[0]
             LOCATION = [LOC_INFO[1], LOC_INFO[2]]
             ID_NAME_LIST = []
-            
                         
             for DATA_HERE in DATA_THIS_ANCESTRY:
                 
@@ -131,9 +106,7 @@ for Box_Size_File in os.listdir(F"{Folder}"):
                 ID1 = PAIR[0]
                 ID2 = PAIR[1]
                 
-                if ( ( ID1 == ID_NAME ) or ( ID2 == ID_NAME ) ) and (DATA_HERE not in SORTED_BY_LOCATION_DATA):
-                    
-                    
+                if ((ID1 == ID_NAME) or (ID2 == ID_NAME)) and (DATA_HERE not in SORTED_BY_LOCATION_DATA):
                     
                     #### Find location of other individual
                     if ID1 == ID_NAME:
@@ -142,184 +115,65 @@ for Box_Size_File in os.listdir(F"{Folder}"):
                     if ID2 == ID_NAME:
                         ID_ALT = ID1
                     
-                    ###3 Cycle through individuals again
+                    ### Cycle through individuals again
                     for ZZ in Individual_Location:
                         ### identify location of 2nd individual in the pair
                         if ID_ALT == ZZ[0]:
                             ## Record their X location
                             LOCATION_V2 = ZZ[1]
                     
-                    
                     ## For second sorting
-                    ID_NAME_LIST.append([DATA_HERE,LOCATION_V2,LOCATION])
+                    ID_NAME_LIST.append([DATA_HERE, LOCATION_V2, LOCATION])
                     
                     SORTED_BY_LOCATION_DATA.append(DATA_HERE)
                     
-                    
-                    
-                    
             ## Second sorting needed for Y axis
-            
             ID_NAME_LIST = sorted(ID_NAME_LIST, key=lambda x: x[1])
-            # print(ID_NAME_LIST)
             
             for TEMP in ID_NAME_LIST:
                 RE_SORTED_BY_LOCATION_DATA.append(TEMP[0])
-                #### print(LOCATION,LOCATION_V2)
-            
-            
-            
         
         Max_Dim = len(SAMPLES)
-        Sim_matrix = np.ones((Max_Dim,Max_Dim))
-
-
+        Sim_matrix = np.ones((Max_Dim, Max_Dim))
 
         FINAL_MATRIX = [x[2] for x in RE_SORTED_BY_LOCATION_DATA]
 
-        
         Box_Size = Box_Size_File.split('.')[0]
         Box_Size = Box_Size.split('_Box_Size_')[1]
-        counter=0
-        for i in range(0,Max_Dim):
+        
+        counter = 0
+        for i in range(0, Max_Dim):
             for j in range(i+1, Max_Dim):
+                Sim_matrix[i, j] = FINAL_MATRIX[counter]
+                Sim_matrix[j, i] = Sim_matrix[i, j]
+                counter += 1
 
-                Sim_matrix[i,j] = FINAL_MATRIX[counter]
-                Sim_matrix[j,i] = Sim_matrix[i,j]
-                counter+=1
-
+        ############## Plotting
         
-
+        fig, ax = plt.subplots(figsize=(8, 6))
         
-        plt.imshow(Sim_matrix, cmap='hot', interpolation='nearest')
-        plt.savefig(F"{Output_Folder}/Composite_Ancestry_{ANC}_Box_Size_{Box_Size}_Similarity_Heatmap.pdf")
+        im = ax.imshow(Sim_matrix, cmap = 'hot', interpolation='nearest')
         
+        cbar = fig.colorbar(im, ax = ax, shrink = 0.8)
+        cbar.set_label('Haplotype Similarity', fontweight='bold', fontsize=10)
+
+        # Extract X, Y location labels from Individual_Location list
+        tick_positions = np.arange(Max_Dim)
+        individual_labels = [f"{int(loc[1]) - int(Box_Size)/2 },{int(loc[2]) - int(Box_Size)/2 }" for loc in Individual_Location]
+
+        ax.set_xticks(tick_positions)
+        ax.set_xticklabels(individual_labels, rotation = 45, fontsize = 8)
         
+        ax.set_yticks(tick_positions)
+        ax.set_yticklabels(individual_labels, fontsize = 8)
+
+        ax.set_title(
+            f'Composite Ancestry {ANC} (Box Size {Box_Size})\nSimilarity Heatmap',
+            fontsize = 12, pad = 14, fontweight = 'bold'
+        )
+        ax.set_xlabel('X, Y Position of Individual', fontweight = 'bold', fontsize = 11)
+        ax.set_ylabel('X, Y Position of Individual', fontweight = 'bold', fontsize = 11)
         
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-##################### Plotting in a 2D space
-
-
-
-#### Create Colour Palette
-# Colours_to_ancestries = {}
-# if len(Genomewide_Ancestries) <= 4:
-    # Colours_to_ancestries = {'1':'xkcd:deep blue','2':'xkcd:pinkish','3':'xkcd:soft green','4':'xkcd:lemon yellow'} ###### https://xkcd.com/color/rgb/
- 
-
- 
-# if len(Genomewide_Ancestries) > 4:
-
-    # for x in range(0,len(Genomewide_Ancestries)):
-        # colorz = tuple(np.random.random(size=3))
-        # Colours_to_ancestries[ Genomewide_Ancestries[x] ] = colorz
-        
-
-
-###################################################################################
-### Organise Individuals in space
-
-
-#### Read info on sampled individuals
-
-# Individual_Info = []
-# Individual_Location = []
-# Individuals_File_Name = Folder.replace("Diversity_Metrics","Sampled_Individuals.txt")
-# Individuals_File = open(Individuals_File_Name,'r')      
-# Ind_Headers = Individuals_File.readline().strip().split()
-
-# for LINE in Individuals_File:
-    # LINE = LINE.strip().split()
-    # Individual_Info.append([ LINE[0], LINE[1], LINE[2] , LINE[3], LINE[4], LINE[5], LINE[6] , LINE[7] ]) #### 
-    # Location = [float(x) for x in LINE[1].split('--')]
-    # Individual_Location.append( [LINE[0]] + Location)
-
-# Individual_Location = sorted(Individual_Location, key = lambda x: x[1]) ### Sorted by position on X axis
-
-
-
-
-
-##### Figure out best width and height for this group of individuals
-# max_width = round(max(Individual_Location, key = lambda x: x[1])[1])### maximum X axis position, rounded
-# max_height = round(max(Individual_Location, key = lambda x: x[2])[2]) ### maximum Y axis position, rounded
-
-## add a bit until its divisable by 5 (arbitary, but makes plots nicer)
-# while max_height%5 !=0:
-    # max_height +=1
-    
-# while max_width%5 !=0:
-    # max_width +=1
-    
-# print(F"Maximum width for this space is: {max_width} and maximum height is: {max_height}\n")
-
-
-
-##### Create spatial boxes of 5x5
-
-# N_X_Boxes = int(max_width/5) ## Number of bins/boxes in X axis
-# N_Y_Boxes = int(max_height/5) ## Number of bins/boxes in Y axis
-# N_Boxes = N_X_Boxes * N_Y_Boxes ## Number of total bins/boxes
-
-##### Seperate all individuals into spatial boxes of 5x5
-
-# BOXES = []
-
-    
-# for X_DIM in range(5,max_width+5,5):
-    # for Y_DIM in range(5,max_height+5,5):
-        
-        # BOX_HERE = []
-        
-        # for IND in Individual_Location:
-            # IND_X = IND[1]
-            # IND_Y = IND[2]
-        
-            # if (IND_X >= X_DIM-5) and (IND_Y >= Y_DIM-5) and (IND_X < X_DIM) and (IND_Y < Y_DIM):
-                
-                # BOX_HERE.append(IND)
-
-        # BOXES.append(BOX_HERE)
-        
-        
-# print(BOXES,len(BOXES))
+        fig.tight_layout()
+        plt.savefig(f"{Output_Folder}/Composite_Ancestry_{ANC}_Box_Size_{Box_Size}_Similarity_Heatmap.pdf")
+        plt.close()
--- Python_Scripts/Plot_Histogram_Matching_Haplotypes_Regions.py
+++ Python_Scripts/Plot_Histogram_Matching_Haplotypes_Regions.py
@@ -9,13 +9,12 @@ import numpy as np
 import itertools
 
 
-
 ########## Load and organise data
 ### Folders
 Folder = sys.argv[1]
 Output_Folder = sys.argv[2]
 File = 'Ancestry_Sharing.txt'
-File = open(F"{Folder}/{File}",'r')
+File = open(f"{Folder}/{File}", 'r')
 
 #### Read first line, get info
 Labels = File.readline().strip().split()
@@ -23,8 +22,7 @@ Labels = File.readline().strip().split()
 Size_of_Box = 5
 
 if len(sys.argv) >= 4:
-    Size_of_Box = int(sys.argv[3])
-
+  Size_of_Box = int(sys.argv[3])
 
 
 ###################### Load data, organise it to pairs of individuals
@@ -32,48 +30,38 @@ if len(sys.argv) >= 4:
 ANCESTRIES = {}
 
 for Line in File:
-    
-    Line = Line.strip().split()
-    
-    ## Load columns
-    Chromosome = Line[0]
-    Ancestry = Line[1]
-    ID1 = Line[2]
-    ID2 = Line[3]
-    ID2 = Line[3]
-    Similarity = float(Line[8])
-    
-    
-    ## Organise
-    
-    IDS = '-'.join(sorted([ID1,ID2]))
-    
-    ### Filter chromosomes
-    if Chromosome in ['chromosome_Y','chromosome_MT']:
-        continue
-    ### not filtered are added into dictionary of ancestries and pairs
-    if Chromosome not in ['chromosome_Y','chromosome_MT']:
-       
-        if Ancestry not in ANCESTRIES.keys():
-            
-            ANCESTRIES[Ancestry] = {IDS:[Similarity]}
-            
-            
-        if Ancestry in ANCESTRIES.keys(): 
-            
-            if IDS not in ANCESTRIES[Ancestry].keys():
-                
-                ANCESTRIES[Ancestry][IDS] = [Similarity]
-            
-            if IDS in ANCESTRIES[Ancestry].keys():
-                
-                ANCESTRIES[Ancestry][IDS].append(Similarity)
 
+  Line = Line.strip().split()
 
+  ## Load columns
+  Chromosome = Line[0]
+  Ancestry = Line[1]
+  ID1 = Line[2]
+  ID2 = Line[3]
+  Similarity = float(Line[8])
 
+  ## Organise
+  IDS = '-'.join(sorted([ID1, ID2]))
 
+  ### Filter chromosomes
+  if Chromosome in ['chromosome_Y', 'chromosome_MT']:
+    continue
+  ### not filtered are added into dictionary of ancestries and pairs
+  if Chromosome not in ['chromosome_Y', 'chromosome_MT']:
 
+    if Ancestry not in ANCESTRIES.keys():
 
+      ANCESTRIES[Ancestry] = {IDS: [Similarity]}
+
+    if Ancestry in ANCESTRIES.keys():
+
+      if IDS not in ANCESTRIES[Ancestry].keys():
+
+        ANCESTRIES[Ancestry][IDS] = [Similarity]
+
+      if IDS in ANCESTRIES[Ancestry].keys():
+
+        ANCESTRIES[Ancestry][IDS].append(Similarity)
 
 
 ######################
@@ -83,20 +71,19 @@ SAMPLES = []
 
 for ANC in ANCESTRIES.keys():
 
-    for IDS in sorted(ANCESTRIES[ANC].keys()):
-        
-        MEAN = np.mean(ANCESTRIES[ANC][IDS])
-        ANCESTRIES[ANC][IDS] = MEAN
-        
-        
-        IDS = IDS.split('-')
-        for ID in IDS:
-            if ID not in SAMPLES:
-                SAMPLES.append(ID)
-                
-        #### Add pair with sorted order
-        IDS = "-".join(sorted(IDS))
-        ANCESTRIES[ANC][IDS] = MEAN
+  for IDS in sorted(ANCESTRIES[ANC].keys()):
+
+    MEAN = np.mean(ANCESTRIES[ANC][IDS])
+    ANCESTRIES[ANC][IDS] = MEAN
+
+    IDS = IDS.split('-')
+    for ID in IDS:
+      if ID not in SAMPLES:
+        SAMPLES.append(ID)
+
+    #### Add pair with sorted order
+    IDS = '-'.join(sorted(IDS))
+    ANCESTRIES[ANC][IDS] = MEAN
 
 
 ############################################
@@ -105,16 +92,27 @@ for ANC in ANCESTRIES.keys():
 
 Individual_Info = []
 Individual_Location = []
-Individuals_File =   open(F"{Folder.replace("Diversity_Metrics","Sampled_Individuals.txt")}",'r')      
+Individuals_File = open(
+    f"{Folder.replace('Diversity_Metrics', 'Sampled_Individuals.txt')}", 'r'
+)
 Ind_Headers = Individuals_File.readline().strip().split()
 
 for LINE in Individuals_File:
-    LINE = LINE.strip().split()
-    Individual_Info.append([ LINE[0], LINE[1], LINE[2] , LINE[3], LINE[4], LINE[5], LINE[6] , LINE[7] ])
-    Location = [float(x) for x in LINE[1].split('--')]
-    Individual_Location.append( [LINE[0]] + Location )
+  LINE = LINE.strip().split()
+  Individual_Info.append([
+      LINE[0],
+      LINE[1],
+      LINE[2],
+      LINE[3],
+      LINE[4],
+      LINE[5],
+      LINE[6],
+      LINE[7],
+  ])
+  Location = [float(x) for x in LINE[1].split('--')]
+  Individual_Location.append([LINE[0]] + Location)
 
-Individual_Location = sorted(Individual_Location, key = lambda x: x[1])
+Individual_Location = sorted(Individual_Location, key=lambda x: x[1])
 
 
 ############################################
@@ -123,31 +121,10 @@ Individual_Location = sorted(Individual_Location, key = lambda x: x[1])
 INDS_TO_HAPLOTYPES = {}
 
 for LOC in Individual_Location:
-    
-    IND = LOC[0]
-    HAPLOTYPES = [ X for X in SAMPLES if F'Individual_{IND}' in X ]
-    INDS_TO_HAPLOTYPES[IND] = HAPLOTYPES
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  IND = LOC[0]
+  HAPLOTYPES = [X for X in SAMPLES if f'Individual_{IND}' in X]
+  INDS_TO_HAPLOTYPES[IND] = HAPLOTYPES
 
 
 ############################################
@@ -155,174 +132,159 @@ for LOC in Individual_Location:
 
 
 ##### Figure out best width and height for this group of individuals
-max_width = round(max(Individual_Location, key = lambda x: x[1])[1]) ### maximum X axis position, rounded
-max_height = round(max(Individual_Location, key = lambda x: x[2])[2]) ### maximum Y axis position, rounded
+max_width = round(
+    max(Individual_Location, key=lambda x: x[1])[1]
+)  ### maximum X axis position, rounded
+max_height = round(
+    max(Individual_Location, key=lambda x: x[2])[2]
+)  ### maximum Y axis position, rounded
 
 ## add a bit until its divisable by 5 (arbitary, but makes plots nicer)
-while max_height % Size_of_Box !=0:
-    max_height+=1
+while max_height % Size_of_Box != 0:
+  max_height += 1
 
-while max_width % Size_of_Box !=0:
-    max_width+=1
+while max_width % Size_of_Box != 0:
+  max_width += 1
 
-print(F"Maximum width for this space is: {max_width} and maximum height is: {max_height}\n")
-
+print(
+    f'Maximum width for this space is: {max_width} and maximum height is:'
+    f' {max_height}\n'
+)
 
 
 ##### Create spatial boxes of 5x5
 
-N_X_Boxes = int(max_width / Size_of_Box) ## Number of bins/boxes in X axis
-N_Y_Boxes = int(max_height / Size_of_Box) ## Number of bins/boxes in Y axis
-N_Boxes = N_X_Boxes * N_Y_Boxes ## Number of total bins/boxes
-
-
-
-
+N_X_Boxes = int(max_width / Size_of_Box)  ## Number of bins/boxes in X axis
+N_Y_Boxes = int(max_height / Size_of_Box)  ## Number of bins/boxes in Y axis
+N_Boxes = N_X_Boxes * N_Y_Boxes  ## Number of total bins/boxes
 
 
 ##### Seperate all individuals into spatial boxes of 5x5 (or whatever your dimentions are)
 
 BOXES = []
 
-    
-for X_DIM in range(Size_of_Box,max_width + Size_of_Box, Size_of_Box): ### left to right
-    for Y_DIM in range(Size_of_Box, max_height + Size_of_Box, Size_of_Box): ## bottom to up
-        
-        BOX_HERE = [ ]
-        
-        for IND in Individual_Location:
-            
-            IND_ID = IND[0]
-            HAPLOTYPES_OF_IND = INDS_TO_HAPLOTYPES[IND_ID]
-            IND_X = IND[1]
-            IND_Y = IND[2]
-            
-            if (IND_X >= X_DIM - Size_of_Box) and (IND_Y >= Y_DIM - Size_of_Box) and (IND_X < X_DIM) and (IND_Y < Y_DIM):## individual within boundaries of box
-                
-                for HAPLT in HAPLOTYPES_OF_IND:
-                    BOX_HERE.append(HAPLT) ### assign individual to this box
-                
-        ### add this 5x5 box to the totality of boxes    
-        BOXES.append([BOX_HERE, X_DIM - Size_of_Box/2, Y_DIM - Size_of_Box/2])
-        
+
+for X_DIM in range(
+    Size_of_Box, max_width + Size_of_Box, Size_of_Box
+):  ### left to right
+  for Y_DIM in range(
+      Size_of_Box, max_height + Size_of_Box, Size_of_Box
+  ):  ## bottom to up
+
+    BOX_HERE = []
+
+    for IND in Individual_Location:
+
+      IND_ID = IND[0]
+      HAPLOTYPES_OF_IND = INDS_TO_HAPLOTYPES[IND_ID]
+      IND_X = IND[1]
+      IND_Y = IND[2]
+
+      if (
+          (IND_X >= X_DIM - Size_of_Box)
+          and (IND_Y >= Y_DIM - Size_of_Box)
+          and (IND_X < X_DIM)
+          and (IND_Y < Y_DIM)
+      ):  ## individual within boundaries of box
+
+        for HAPLT in HAPLOTYPES_OF_IND:
+          BOX_HERE.append(HAPLT)  ### assign individual to this box
+
+    ### add this 5x5 box to the totality of boxes
+    BOXES.append([BOX_HERE, X_DIM - Size_of_Box / 2, Y_DIM - Size_of_Box / 2])
 
 
-
-
-fig, ax = plt.subplots()
-
-box_labels = [F"{int(box[1])},{int(box[2])}" for box in BOXES]
-
+box_labels = [f'{int(box[1])},{int(box[2])}' for box in BOXES]
 
 
 ##### For each ancestry
 
 for Ancestry in sorted(ANCESTRIES.keys()):
 
-    PAIRED_BOXES = []
-    PAIRED_BOXES_LOCATION = []
+  PAIRED_BOXES = []
+  PAIRED_BOXES_LOCATION = []
 
-    ###### Compare every haplosome in box 1 with every haplosome in box 2
-    for BOX_1 in BOXES:
-        for BOX_2 in BOXES:
-            
-            HAPS_BOX1 = BOX_1[0]
-            X_OF_BOX_1 = int(BOX_1[1])
-            Y_OF_BOX_1 = int(BOX_1[2])
-            
-            HAPS_BOX2 = BOX_2[0]
-            X_OF_BOX_2 = int(BOX_2[1])
-            Y_OF_BOX_2 = int(BOX_2[2])     
-            
+  ###### Compare every haplosome in box 1 with every haplosome in box 2
+  for BOX_1 in BOXES:
+    for BOX_2 in BOXES:
 
-            ALL_PAIRS = list(itertools.product(HAPS_BOX1, HAPS_BOX2))
-            ALL_PAIRS = [sorted(list(x)) for x in ALL_PAIRS]
-            
-            
-            MEAN_SIMILARITY_BOX = []
-                
-            for PAIR in ALL_PAIRS:
-                
-                if PAIR[0] != PAIR[1]:
-                    IDS = '-'.join(PAIR)
-                    MEAN_SIMILARITY_BOX.append(ANCESTRIES[Ancestry][IDS])
-            
-            if MEAN_SIMILARITY_BOX == []:
-                MEAN_SIMILARITY_BOX.append(0)
-                
-            MEAN_SIMILARITY_BOX = np.mean(MEAN_SIMILARITY_BOX)
-            
-            PAIRED_BOXES.append(MEAN_SIMILARITY_BOX) ####  X_OF_BOX_1, Y_OF_BOX_1, X_OF_BOX_2, Y_OF_BOX_2
-            PAIRED_BOXES_LOCATION.append(F"{X_OF_BOX_1}:{Y_OF_BOX_1}\n{X_OF_BOX_2}:{Y_OF_BOX_2}") ####  X_OF_BOX_1, Y_OF_BOX_1, X_OF_BOX_2, Y_OF_BOX_2
-            
-            
-            
-    Max_Dim = len(BOXES)
-    Sim_matrix = np.zeros((Max_Dim,Max_Dim))
-    
-    FINAL_MATRIX = PAIRED_BOXES
-    
-    counter=0
-    for i in range(0,Max_Dim):
-        for j in range(0, Max_Dim):
-            
-            Sim_matrix[i,j] = FINAL_MATRIX[counter]
-            Sim_matrix[j,i] = Sim_matrix[i,j]
-            
-            
-            ### Add location of each pair
-            Locations = PAIRED_BOXES_LOCATION[counter]
-            ### ax.text(j, i, Locations, ha='center',va='center',color='black',fontsize=0.05,rotation=-45,rotation_mode='anchor')
-            counter+=1
-    
-    
-    # plt.xticks([x + 0.5 for x in range(0, Max_Dim)], [x for x in range(Size_of_Box, Size_of_Box * Max_Dim + Size_of_Box, Size_of_Box)], rotation=45)
-    # plt.yticks([x + 0.5 for x in range(0, Max_Dim)], [x for x in range(Size_of_Box, Size_of_Box * Max_Dim + Size_of_Box, Size_of_Box)])
-    
-    ### Ticks to middle of each box
-    tick_positions = np.arange(Max_Dim)
-    # Apply Xaxis ticks and labels
-    ax.set_xticks(tick_positions)
-    ax.set_xticklabels(box_labels, rotation=45, fontsize=8)
-    
-    # Apply Yaxis ticks and labels
-    ax.set_yticks(tick_positions)
-    ax.set_yticklabels(box_labels, fontsize=8)
+      HAPS_BOX1 = BOX_1[0]
+      X_OF_BOX_1 = int(BOX_1[1])
+      Y_OF_BOX_1 = int(BOX_1[2])
 
-    
-    plt.xlabel('X, Y position of Box', fontweight ='bold', fontsize = 13)
-    plt.ylabel('X, Y position of Box', fontweight ='bold', fontsize = 13)
-    plt.tight_layout()
-    plt.title(F'Heatmap of similarity of\n Ancestry {Ancestry} segments\n grouped in Boxes of Size {Size_of_Box}', fontsize = 12, pad = 14, fontweight ='bold')
-    plt.imshow(Sim_matrix, cmap = 'hot', interpolation = 'nearest')
-    
-    
-    
-    
-    
-    plt.savefig(F"{Output_Folder}/Ancestry_{Ancestry}_Regional_Similarity_Heatmap_boxsize_{Size_of_Box}.pdf")
-    
-    
-    
-    ########### Distance Matrix with Dendogram
-    fig, ax = plt.subplots()
-    
-    tick_positions = np.arange(Max_Dim)
-    #####3# Apply Xaxis ticks and labels
-    ax.set_xticks(tick_positions)
-    ax.set_xticklabels(box_labels, rotation=45, fontsize=8)
-    
-    ######  Apply Yaxis ticks and labels
-    ax.set_yticks(tick_positions)
-    ax.set_yticklabels(box_labels, fontsize=8)
-    
-    
-    plt.xlabel('X, Y position of Box', fontweight ='bold', fontsize = 13)
-    plt.ylabel('X, Y position of Box', fontweight ='bold', fontsize = 13)
-    plt.tight_layout()
-    plt.title(F'Heatmap of similarity of\n Ancestry {Ancestry} segments\n grouped in Boxes of Size {Size_of_Box}', fontsize = 12, pad = 14, fontweight ='bold')
-    plt.imshow(Sim_matrix, cmap = 'hot', interpolation = 'nearest')
-    
-    
-    
-    plt.savefig(F"{Output_Folder}/Ancestry_{Ancestry}_Regional_Similarity_Heatmap_boxsize_{Size_of_Box}.pdf")
+      HAPS_BOX2 = BOX_2[0]
+      X_OF_BOX_2 = int(BOX_2[1])
+      Y_OF_BOX_2 = int(BOX_2[2])
+
+      ALL_PAIRS = list(itertools.product(HAPS_BOX1, HAPS_BOX2))
+      ALL_PAIRS = [sorted(list(x)) for x in ALL_PAIRS]
+
+      MEAN_SIMILARITY_BOX = []
+
+      for PAIR in ALL_PAIRS:
+
+        if PAIR[0] != PAIR[1]:
+          IDS = '-'.join(PAIR)
+          MEAN_SIMILARITY_BOX.append(ANCESTRIES[Ancestry][IDS])
+
+      if MEAN_SIMILARITY_BOX == []:
+        MEAN_SIMILARITY_BOX.append(0)
+
+      MEAN_SIMILARITY_BOX = np.mean(MEAN_SIMILARITY_BOX)
+
+      PAIRED_BOXES.append(
+          MEAN_SIMILARITY_BOX
+      )  ####  X_OF_BOX_1, Y_OF_BOX_1, X_OF_BOX_2, Y_OF_BOX_2
+      PAIRED_BOXES_LOCATION.append(
+          f'{X_OF_BOX_1}:{Y_OF_BOX_1}\n{X_OF_BOX_2}:{Y_OF_BOX_2}'
+      )  ####  X_OF_BOX_1, Y_OF_BOX_1, X_OF_BOX_2, Y_OF_BOX_2
+
+  Max_Dim = len(BOXES)
+  Sim_matrix = np.zeros((Max_Dim, Max_Dim))
+
+  FINAL_MATRIX = PAIRED_BOXES
+
+  counter = 0
+  for i in range(0, Max_Dim):
+    for j in range(0, Max_Dim):
+
+      Sim_matrix[i, j] = FINAL_MATRIX[counter]
+      Sim_matrix[j, i] = Sim_matrix[i, j]
+
+      ### Add location of each pair
+      Locations = PAIRED_BOXES_LOCATION[counter]
+      counter += 1
+
+  fig, ax = plt.subplots(figsize=(8, 6))
+
+  im = ax.imshow(Sim_matrix, cmap='hot', interpolation='nearest')
+
+  cbar = fig.colorbar(im, ax=ax, shrink=0.8)
+  cbar.set_label('Haplotype Similarity', fontweight='bold', fontsize=10)
+
+  ### Ticks to middle of each box
+  tick_positions = np.arange(Max_Dim)
+
+  # Apply Xaxis ticks and labels
+  ax.set_xticks(tick_positions)
+  ax.set_xticklabels(box_labels, rotation = 90, fontsize = 4)
+
+  # Apply Yaxis ticks and labels
+  ax.set_yticks(tick_positions)
+  ax.set_yticklabels(box_labels, fontsize = 4)
+
+  ax.set_xlabel('X, Y position of Box', fontweight='bold', fontsize=13)
+  ax.set_ylabel('X, Y position of Box', fontweight='bold', fontsize=13)
+  ax.set_title(
+      f'Heatmap of similarity of\n Ancestry {Ancestry} segments\n grouped in'
+      f' Boxes of Size {Size_of_Box}',
+      fontsize=12,
+      pad=14,
+      fontweight='bold',
+  )
+
+  fig.tight_layout()
+  plt.savefig(
+      f'{Output_Folder}/Ancestry_{Ancestry}_Regional_Similarity_Heatmap_boxsize_{Size_of_Box}.pdf'
+  )
+  plt.close()
